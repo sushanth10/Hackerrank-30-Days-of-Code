813dd123fa8383275866a15befcea0b373aeeca0
Sushanth Bhat <sushanthbhat15@gmail.com>
2021-05-13 10:08:03 +0530

    day 19 completed

---
+++ Day 19 - Interfaces.py
@@ -0,0 +1,18 @@
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        sum_div = n
+        for i in range(1,n//2+1):
+            if(n%i)==0:
+                sum_div += i
+        return sum_div
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
